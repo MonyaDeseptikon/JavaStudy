b0d7a77771754a9140f1a3d358d7dce3cb5d0ea4
Monya <isadom@mail.ru>
2024-07-07 13:25:11 +0400

    Java Core Домашнее задание №4

---
+++ JCHW5/src/MainJCHW5.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+public class MainJCHW5 {
+    public static void main(String[] args) throws IOException {
+        Backup backup = new Backup(".");
+        backup.copy();
+
+    }
+}
